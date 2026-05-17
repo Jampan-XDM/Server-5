47fa1fd79390535288e47c44178d4861a1249a91
Jampan-XDM <jampankelvin@gmail.com>
2026-05-17 11:45:57 +0300

    Create pair.js

---
+++ pair.js
@@ -0,0 +1,7 @@
+// pair.js sasa inatumika tu kama router, index.js ndio injini kuu
+module.exports = { 
+    startPairing: async (number) => {
+        // Huu ni mwito tu, index itashughulikia kila kitu
+        return "PROCESSING"; 
+    }
+};
